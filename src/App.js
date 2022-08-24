68372633a57fdb581c959f60597083bb5cdeaffb
Sebas Sotelo <sebassotelo97@gmail.com>
2022-08-24 04:15:49 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,109 @@
-import React, { useEffect , useState} from 'react';
-import './App.css';
-import Card from './componentes/card/card';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./componentes/card/card";
 
 function App() {
+  const [peliculas, setPeliculas] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pag, setPag] = useState(1);
+  const [genero, setGenero] = useState("popular");
 
-    const [peliculas, setPeliculas] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [pag, setPag] = useState(1);
-    const [genero, setGenero] = useState('popular')
+  const pelis = async () => {
+    try {
+      setIsLoading(true);
+      const pelis = await fetch(
+        `https://api.themoviedb.org/3/movie/${genero}?api_key=e8002c635aacc458eb931614052b44af&language=es-MX&page=${pag}&videos`
+      );
+      const datos = await pelis.json();
+      setPeliculas(
+        datos.results.map((pelicula) => ({
+          title: pelicula.title,
+          img: pelicula.poster_path,
+          id: pelicula.id,
+          desc: pelicula.overview,
+          puntaje: pelicula.vote_average,
+          estreno: pelicula.release_date,
+        }))
+      );
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-
-      const pelis = async() =>{
-        try {
-          setIsLoading(true);
-          const pelis = await fetch(`https://api.themoviedb.org/3/movie/${genero}?api_key=e8002c635aacc458eb931614052b44af&language=es-MX&page=${pag}`);
-          const datos = await pelis.json();
-
-          console.log(datos.results)
-          setPeliculas(datos.results.map((pelicula) => ({
-            title: pelicula.title,
-            img: pelicula.poster_path
-          })) );
-          
-          
-        }catch(err){
-          console.log(err);
-
-        }finally{
-          setIsLoading(false)
-        }
-        
-        
-      }
-
-      
-
-  useEffect(()=> {
+  useEffect(() => {
     pelis();
-    console.log(pag)
-  },[pag,genero])
+  }, [pag, genero]);
 
-  const pagSiguiente = () =>{
-    if(pag < 1000){
-      setPag(pag + 1)
-      setIsLoading(false)
+  const pagSiguiente = () => {
+    if (pag < 1000) {
+      setPag(pag + 1);
+      setIsLoading(false);
     }
-  }
-  const pagAnterior = () =>{
-    if(pag > 1){
-      setPag(pag - 1)
-      setIsLoading(false)
+  };
+  const pagAnterior = () => {
+    if (pag > 1) {
+      setPag(pag - 1);
+      setIsLoading(false);
     }
-    
-  }
+  };
 
-  const pelisPuntuadas= () =>{
-    setGenero('top_rated')
-    setPag(1)
-  }
-  const pelisPopulares= () =>{
-    setGenero('popular')
-    setPag(1)
-  }
-  const pelisProx= () =>{
-    setGenero('upcoming')
-    setPag(1)
-  }
-
-
+  const pelisPuntuadas = () => {
+    setGenero("top_rated");
+    setPag(1);
+  };
+  const pelisPopulares = () => {
+    setGenero("popular");
+    setPag(1);
+  };
+  const pelisProx = () => {
+    setGenero("now_playing");
+    setPag(1);
+  };
 
   return (
     <div className="App">
-      <div className='navbar'>
-        <ul className='nav__ul'>
-          <li className='nav__li' onClick={pelisPuntuadas}><p>Mejor Puntuadas</p></li>
-          <li className='nav__li' onClick={pelisPopulares}><p>Populares</p></li>
-          <li className='nav__li' onClick={pelisProx}><p>Proximamente</p></li>
+      <div className="navbar">
+        <ul className="nav__ul">
+          <li className="nav__li" onClick={pelisPuntuadas}>
+            <p>Mejor Puntuadas</p>
+          </li>
+          <li className="nav__li" onClick={pelisPopulares}>
+            <p>Populares</p>
+          </li>
+          <li className="nav__li" onClick={pelisProx}>
+            <p>Estrenos</p>
+          </li>
         </ul>
       </div>
-      <div className='app__pelis'>
-         {isLoading === true ? <p>Loading</p> : peliculas?.map((m,i) => <Card title={m.title} img= {m.img} key={`k__${i}`}/> )}
+      <div className="app__pelis">
+        {isLoading === true ? (
+          <p>Loading</p>
+        ) : (
+          peliculas?.map((m, i) => (
+            <Card
+              title={m.title}
+              img={m.img}
+              id={m.id}
+              key={`k__${i}`}
+              desc={m.desc}
+              puntaje={m.puntaje}
+              estreno={m.estreno}
+            />
+          ))
+        )}
       </div>
-      
 
-
-      
-      <div className='btn__pagina'>
-        <div className='btn__pagina__container'>
-          <button className='btn' onClick={pagAnterior}>Anterior</button>
+      <div className="btn__pagina">
+        <div className="btn__pagina__container">
+          <button className="btn" onClick={pagAnterior}>
+            Anterior
+          </button>
           <p>{pag}</p>
-          <button className='btn' onClick={pagSiguiente}>Siguiente</button>
+          <button className="btn" onClick={pagSiguiente}>
+            Siguiente
+          </button>
         </div>
       </div>
     </div>
--- src/componentes/card/card.jsx
+++ src/componentes/card/card.jsx
@@ -1,20 +1,127 @@
-import React, { useState } from 'react'
-import './card.css';
-import Popup from '../popup/popup';
+import React, { useState, useEffect } from "react";
+import "./card.css";
+import ReactPlayer from "react-player";
+import { IoIosCloseCircle } from "react-icons/io";
+import { GoUnmute, GoMute } from "react-icons/go";
 
-function Card({title , img }) {
+function Card({ title, img, id, desc, puntaje, estreno }) {
+  const [show, setShow] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+  const [link, setLink] = useState("");
+  const [mute, setMute] = useState(false);
+  const [descrip, setDescrip] = useState(false);
 
+  const popUp = () => {
+    if (link !== "" || desc !== "") {
+      setShow(!show);
+    }
+  };
 
+  const videos = async () => {
+    try {
+      const video = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=e8002c635aacc458eb931614052b44af&language=en-US`
+      );
+      const datosVideo = await video.json();
+
+      const trailer = datosVideo.results
+        .filter((tra) => tra.type.toLowerCase().includes("trailer"))
+        .map((elem) => ({
+          llave: elem.key,
+        }));
+      // const trailer = datosVideo.results
+      //   .filter((tra) => tra.type.toLowerCase().includes("trailer"))
+      //   .map((elem) => ({
+      //     llave: elem.key,
+      //   }));
+
+      // setVid(trailer.map((elem)=> ({
+      //   llave: elem.key
+      // })))
+
+      setLink(trailer[0].llave);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sonido = () => {
+    setMute(!mute);
+  };
+
+  useEffect(() => {
+    videos();
+
+    if (link !== "") {
+      setShowCard(true);
+    }
+
+    if (desc !== "") {
+      setDescrip(true);
+    }
+  }, [show]);
 
   return (
-    <div>
-        <div className='card-container'>
-          <div className='title'><h1>{title}</h1></div>
-          <img src={`https://image.tmdb.org/t/p/w500/${img}`} alt="" />
-        </div>  
+    <div className={show ? "card" : ""}>
+      <div
+        className={show ? "card-container-not" : "card-container"}
+        onClick={popUp}
+      >
+        <div className="title">
+          <h1>{title}</h1>
+        </div>
+        <img src={`https://image.tmdb.org/t/p/w500/${img}`} alt="" />
+      </div>
 
+      {show && (
+        <div className={show ? "popup-container" : "popup-not"}>
+          {show ? (
+            <div className="video">
+              {showCard ? (
+                <ReactPlayer
+                  url={`https://www.youtube.com/watch?v=${link}`}
+                  height={"100%"}
+                  width={"100%"}
+                  volume={0.05}
+                  playing={true}
+                  muted={mute}
+                />
+              ) : (
+                <h4 className="no-video">NO HAY VIDEO</h4>
+              )}
+            </div>
+          ) : (
+            ""
+          )}
+          <div className={descrip ? "desc" : "no-desc"}>
+            <div className="desc-cont">
+              <p className="desc-estreno">Estreno: {estreno}</p>
+              <p className="desc-puntaje">{puntaje}</p>
+            </div>
+            <h3 className="desc-title">{title}</h3>
+            <p className="desc-desc">{desc}</p>
+
+            {mute ? (
+              <GoMute className="desc-mute" onClick={sonido} />
+            ) : (
+              <GoUnmute className="desc-mute" onClick={sonido} />
+            )}
+            <IoIosCloseCircle className="desc-close" onClick={popUp} />
+          </div>
+        </div>
+      )}
+
+      {/* <div className={show ? 'popup-container' : 'popup-not'} onClick={popUp}>
+            {show ? <div className='video'> 
+                      <ReactPlayer url={`https://www.youtube.com/watch?v=${link}`} height={'100%'} width={'100%'} playing={false} controls={false} volume={0.05} playing={true}/> 
+                      </div> 
+                      : ''}
+
+               
+              <h3>{title}</h3>
+        </div>  */}
     </div>
-  )
+  );
 }
 
 export default Card;
